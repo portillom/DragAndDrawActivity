f8758ece40a21eb4aeaf79db8d2dd058330e12cd
USER <portillom@g.cofc.edu>
2019-01-12 21:17:51 -0800

    Initial commit

--- app/src/main/java/com/michaelportillo/android/draganddrawactivity/BoxDrawingView.java
+++ app/src/main/java/com/michaelportillo/android/draganddrawactivity/BoxDrawingView.java
@@ -1,7 +1,10 @@
 package com.michaelportillo.android.draganddrawactivity;
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -9,6 +12,8 @@ import android.view.View;
  */
 
 public class BoxDrawingView extends View {
+
+    private static final String TAG = "BoxDrawingView";
 
     //Used when creating the view in code
     public BoxDrawingView(Context context){
@@ -18,5 +23,30 @@ public class BoxDrawingView extends View {
     //Used when inflating the view from XML
     public BoxDrawingView(Context context, AttributeSet attrs){
         super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        PointF current = new PointF(event.getX(), event.getY());
+        String action = "";
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                break;
+        }
+
+        Log.i(TAG, action + " at x=" + current.x + ", y=" + current.y);
+
+        return true;
     }
 }
